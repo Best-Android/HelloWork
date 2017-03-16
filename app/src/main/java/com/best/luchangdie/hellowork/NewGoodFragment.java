cea5734c1e402d437469721c444f5719f46b7aa4
luchangdie <1403105250@qq.com>
2017-03-16 13:51:54 +0800

    NewGoodFragment analysis json data

--- app/src/main/java/com/best/luchangdie/hellowork/NewGoodFragment.java
+++ app/src/main/java/com/best/luchangdie/hellowork/NewGoodFragment.java
@@ -3,18 +3,23 @@ package com.best.luchangdie.hellowork;
 
 
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.best.luchangdie.hellowork.bean.PicturesData;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewGoodFragment extends Fragment {
-
+    String url = "http://gank.io/api/data/Android/10/1";
 
     public NewGoodFragment() {
         // Required empty public constructor
@@ -25,7 +30,18 @@ public class NewGoodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        OkHttpUtils.get().url(url).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                PicturesData pictures = new PicturesData();
+                pictures.parseJson(response);
+            }
+        });
         return inflater.inflate(R.layout.fragment_new_good, container, false);
     }
-
 }
