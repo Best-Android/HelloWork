abc4a6b57cae0fa07bab3002b9a2f2cf776b050b
luchangdie <1403105250@qq.com>
2017-03-23 17:38:01 +0800

    create WebViewActivity and receive url

---
+++ app/src/main/java/com/best/luchangdie/hellowork/WebViewActivity.java
@@ -0,0 +1,23 @@
+package com.best.luchangdie.hellowork;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @BindView(R.id.webView)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        ButterKnife.bind(this);
+        String url = getIntent().getStringExtra("url");
+        webView.loadUrl(url);
+    }
+}
